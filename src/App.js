7124dc5272ffe062a5303e543ebea2b6f0db10e2
root <root@pblap516.etechaces.com>
2018-06-12 17:29:59 +0530

    Added code for show hide data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import Person from './Person/Person';
 import './App.css';
+import './Person/Person.css';
 
 class App extends Component {
 
@@ -9,7 +10,8 @@ class App extends Component {
     persons:[
       {Name:"Mohit Sachdeva",Age:"27"},
       {Name:"Rakesh Kumar",Age:"29"}
-    ]
+    ],
+    showPersonList : false
   }
 
   switchNameHandler = (newName) => {
@@ -32,11 +34,18 @@ class App extends Component {
 
   }
 
+  showPersonHandler = () => {
+    const currentBoolValue =  this.state.showPersonList;
+    this.setState({showPersonList: !currentBoolValue})
+  }
+
   render() {
     return (
       <div className="App">
        <h1>Hello I am a React Page.</h1>
-       <button onClick={this.switchNameHandler.bind(this,'Mohit')}>Switch Name</button>
+       <button onClick={this.showPersonHandler}>Switch Name</button>
+       { this.state.showPersonList ?
+         <div>
        <p><Person
             Name = {this.state.persons[0].Name} Age = {this.state.persons[0].Age} Change = {this.nameChangedHandler} Click = {this.switchNameHandler.bind(this,"Mohit Sachdeva")}
           /></p>
@@ -44,6 +53,9 @@ class App extends Component {
             Name = {this.state.persons[1].Name} Age = {this.state.persons[1].Age} >I am from Delhi.
           </Person>
        </p>
+       </div> : null
+     }
+
       </div>
     );
   }
